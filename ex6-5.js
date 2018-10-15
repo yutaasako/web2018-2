8ee16c7f19f7196d35ca86e81fec7f0b58484480
zm17002 <zm17002@josai.ac.jp>
2018-10-14 21:51:17 -0400

    1015

--- ex6-5.js
+++ ex6-5.js
@@ -1,3 +1,3 @@
-function sayhello65(){var who65='world';alert('Hello,'+who65+'!');}
-function taro(){who65='太郎';}
-function hanako(){who65='花子';}
+var who65='world';function sayhello65(){alert('Hello,'+who65+'!');}
+function taro65(){who65='太郎';}
+function hanako65(){who65='花子';}
---
+++ ex6-7.js
@@ -0,0 +1,2 @@
+var who='world';function sayhello67(){alert('Hello,'+who+'!');}
+function someone(x){who=x;}
